6734d15f5071a5d2cbf0e9bddf7995f18e03804f
abhinav-TB <abhinavtb@gmail.com>
2020-03-22 17:51:20 +0530

    chaged to keroku route

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // make connection 
-var socket=io.connect('http://localhost:4000')
+var socket=io.connect('https://chat-room3.herokuapp.com/')
   
 var message= document.getElementById('message'),
     handle=document.getElementById('handle'),

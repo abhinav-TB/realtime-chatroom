ae8d0fb0c3ff66684387a2f0f64ccaaf0978f902
abhinav-TB <abhinavtb@gmail.com>
2020-03-22 17:36:31 +0530

    typing action implemented

--- public/chat.js
+++ public/chat.js
@@ -4,7 +4,8 @@ var socket=io.connect('http://localhost:4000')
 var message= document.getElementById('message'),
     handle=document.getElementById('handle'),
     btn=document.getElementById('send'),
-    output=document.getElementById('output');
+    output=document.getElementById('output'),
+    feedback=document.getElementById('feedback')
 // emit events
 
 btn.addEventListener('click',function(){
@@ -13,9 +14,17 @@ btn.addEventListener('click',function(){
         handle:handle.value
     });
 })
+message.addEventListener('keypress',function(){
+    socket.emit('typing',handle.value);
+})
 
 // listen for events
 socket.on('chat',function(data){
+    feedback.innerHTML=""
     output.innerHTML+='<p><strong>'+data.handle+':</strong>'+ data.message+'</p>';
 })
 
+
+socket.on('typing',function(data){
+    feedback.innerHTML='<p><em>'+data+'is typing a message...</em></p>';
+})

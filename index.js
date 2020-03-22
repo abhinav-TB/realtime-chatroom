efdf9df1f42915b63ffe95ab71cc7b785d2ccadd
abhinav-TB <abhinavtb@gmail.com>
2020-03-22 15:58:18 +0530

    emitted messages

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express=require('express')
+var socket=require('socket.io')
 
 var app= express();
 var server=app.listen(4000,function(){
@@ -6,3 +7,14 @@ var server=app.listen(4000,function(){
 });
 
 app.use(express.static('public'));
+
+var io=socket(server);
+
+io.on('connection',function(socket){
+    console.log("made socket connection")
+    console.log(socket.id)
+    socket.on('chat',function(data){
+        io.sockets.emit('chat',data)
+    })
+})
+
